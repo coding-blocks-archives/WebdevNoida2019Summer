8af00ae35c8c51326bd78b4cd2a9abf020cfe5be
aayusharora <aayush113002@gmail.com>
2019-06-27 16:48:58 +0530

    Added lecture-9

---
+++ Lecture-9/app.js
@@ -0,0 +1,34 @@
+
+function readFile(name, cb) {
+    // Reading the text from the file
+    setTimeout(function(){
+        let data = 'Hello World';
+        cb(data)
+    },0) 
+}
+function convertToString(data, cb) {
+  setTimeout(function(){
+    let strin = data.toString();
+    cb(strin);
+  },0)  
+}
+function splittingData(str, cb) {
+    setTimeout(function(){
+        let arr = str.split('');
+        cb(arr);
+    },0)
+  
+}
+function getRidofSpace(str, cb) {
+    setTimeout(function(){
+        let s = str.replace(/\s/g, '');
+        cb(s);
+    },0)
+}
+function loop(arr, cb){
+    setTimeout(function(){
+        arr.forEach(function(data){
+            cb(data);
+        });
+    },0)
+}
---
+++ Lecture-9/main.js
@@ -0,0 +1,14 @@
+
+readFile('main.txt', function(data){
+    convertToString(data, function(td){
+        getRidofSpace(td, function(ws){
+            splittingData(ws, function(arr){
+                loop(arr,function(data){
+                    console.log(data);
+                })
+            })
+        })
+    })
+})
+
+// oRDER COLDDRINK
